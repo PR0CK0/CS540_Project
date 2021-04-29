084e85e6be5abf35f5747765748a75ee27925c16
Psychobagger <31494145+Psychobagger@users.noreply.github.com>
2021-04-29 10:45:34 -0400

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,5 @@
+create table volusia.parcel_elev
+(parid double precision, taxyr double precision, rolltype text, acctype text, dorid text, alt_id text, luc text, luc_desc text, taxdist text, taxdist_desc text, nbhd text,
+nbhd_desc text, mararea text, mararea_desc text, aprland double precision, aprbldg double precision, aprtot double precision, newconst double precision, additions double precision, 
+deletions double precision, sasd double precision, nsasd double precision, stxbl double precision, nstxbl double precision, cotxbl double precision, citxbl double precision, 
+cra text, cur text, dtcreated text, naics text, naics_desc text, eqval double precision, penval double precision, livunit text, hx_flag text, parcel_elevation integer);
