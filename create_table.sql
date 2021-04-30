8cb681062bdf132117ae83dd4e118309fb2e37cf
Psychobagger <31494145+Psychobagger@users.noreply.github.com>
2021-04-30 11:04:26 -0400

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,16 +1,1 @@
-drop table if exists volusia.parcel_elev;
 
-create table volusia.parcel_elev
-(parid double precision, taxyr double precision, rolltype text, acctype text, dorid text, alt_id text, luc text, luc_desc text, taxdist text, taxdist_desc text, nbhd text,
-nbhd_desc text, mararea text, mararea_desc text, aprland double precision, aprbldg double precision, aprtot double precision, newconst double precision, additions double precision, 
-deletions double precision, sasd double precision, nsasd double precision, stxbl double precision, nstxbl double precision, cotxbl double precision, citxbl double precision, 
-cra text, cur text, dtcreated text, naics text, naics_desc text, eqval double precision, penval double precision, livunit text, hx_flag text, parcel_elevation integer);
-
-
-
-
-
-drop table if exists volusia.contours_analysis2;
-
-create table volusia.contours_analysis2
-(parid double precision, elev integer);
